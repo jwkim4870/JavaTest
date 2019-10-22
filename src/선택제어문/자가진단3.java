5df42071b8737a1cddda4dbc1715f18d204597ab
hkedu <hkedu@192.168.6.129>
2019-10-22 18:36:28 +0900

    commit test2

--- src/선택제어문/자가진단3.java
+++ src/선택제어문/자가진단3.java
@@ -7,9 +7,13 @@ public class 자가진단3 {
     그렇지 않으면 몇 년후에 성인이 되는지를 "○ years later"라는 메시지를 출력하는 프로그램을 작성하시오.*/
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("나이를 입력하세요.");
 		int age = sc.nextInt();
 		if(age > 20) {
-			System.out.println();
+			System.out.println("adult");
+		} else if(age < 20) {
+			int diff = 20 - age;
+			System.out.println(diff + " years later.");
 		}
 	}
 
---
+++ src/선택제어문/자가진단4.java
@@ -0,0 +1,10 @@
+package 선택제어문;
+
+public class 자가진단4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
